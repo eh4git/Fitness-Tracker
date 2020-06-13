06d859b37dce1a790a9286040c548b2542503d0e
erik hirsch <ehirsch760@gmail.com>
2020-06-13 12:23:57 -0700

    set up routes, models, and server.

---
+++ routes/api.js
@@ -0,0 +1,63 @@
+const router = require("express").Router();
+const Workout = require("../models/workout");
+
+
+router.get("/api/workouts", function(req, res){
+    Workout.find()
+        .then(function(dbWorkout) {
+            console.log(dbWorkout)
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err)
+        })
+});
+
+router.get("/api/workouts/range", function(req, res){
+    Workout.find({}).limit(7)
+        .then(function(dbWorkout) {
+            console.log(dbWorkout)
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err)
+        })
+});
+
+router.post("/api/workouts", function(req, res){
+    Workout.create({})
+        .then(function(dbWorkout) {
+            console.log(dbWorkout)
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err)
+        })
+});
+
+router.delete("/api/workouts", function({ body }, res){
+    Workout.findByIdAndDelete(body.id)
+        .then(function() {
+            res.json(true);
+        })
+        .catch(err => {
+            res.json(err)
+        })
+});
+
+router.put("/api/workouts", function({ body, params }, res){
+    Workout.findByIdAndUpdate(
+        params.id,
+        { $push: { exercises: body } },
+        // "runValidators" will ensure new exercises meet our schema requirements
+        { new: true, runValidators: true }
+    )
+        .then(function() {
+            res.json(true);
+        })
+        .catch(err => {
+            res.json(err)
+        })
+});
+
+module.exports = router;
